5cd6989220dddef654d090b5c2940362d36c9958
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2023-06-17 18:04:49 -0300

    Página não encontrada

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,4 +14,10 @@ if (in_array($uri, array_keys($routes))) {
 
 	Route::$method($uri, $routes[$uri]);
 
+} else {
+
+	header($_SERVER["SERVER_PROTOCOL"] . "/1.0 404 Not Found", true, 404);
+
+	echo "<h1>Not Found</h1>";
+
 }
