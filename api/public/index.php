31c73a539e4dc28fcf431954920ab99fd9b16747
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2023-06-17 17:58:11 -0300

    Utilizando o autoload do composer

--- api/public/index.php
+++ api/public/index.php
@@ -2,16 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register(function ($className) {
-
-	$pathFile = str_replace('\\', DIRECTORY_SEPARATOR, str_replace('App', 'app', $className)) . '.php';
-
-	if (file_exists(realpath(__DIR__ . '/../' . $pathFile))) {
-		require_once realpath(__DIR__ . '/../' . $pathFile);
-	}
-
-});
-
 if (file_exists(__DIR__ . '/../routes/api.php')) {
 	require __DIR__ . '/../routes/api.php';
 }
